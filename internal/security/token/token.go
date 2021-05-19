643dbcb6b204740d2492793cbe203de979c13557
Achmad Chun-Chun Winata Adi <anonychun@gmail.com>
2021-05-20 01:22:26 +0700

    refactor: change variable names

--- internal/security/token/token.go
+++ internal/security/token/token.go
@@ -7,18 +7,13 @@ import (
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
-type TokenModel interface {
+type Generator interface {
 	GenerateClaims() jwt.MapClaims
 }
 
-func GenerateToken(model TokenModel) (string, error) {
-	claims := model.GenerateClaims()
+func GenerateToken(g Generator) (string, error) {
+	claims := g.GenerateClaims()
 	claims["exp"] = time.Now().Add(config.Cfg().JwtTTL).Unix()
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(config.Cfg().JwtSecretKey))
-}
-
-func ExtractClaims(tokenString string) jwt.MapClaims {
-	tokenParse, _ := jwt.Parse(tokenString, nil)
-	return tokenParse.Claims.(jwt.MapClaims)
 }
